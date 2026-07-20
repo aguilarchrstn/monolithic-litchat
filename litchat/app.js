686f8077c51c63a0021922ebc3d34f9e5c67a6ee
aguilarchrstn <aguilarchan1215@gmail.com>
2026-07-20 15:16:35 +0800

    Change brand name from 'Litchat Test' to 'Litchat Staging'

--- litchat/app.js
+++ litchat/app.js
@@ -738,7 +738,7 @@ function getHTML() {
   <div id="sidebar">
     <div class="brand">
       <div class="brand-logo">💬</div>
-      <div class="brand-name">Litchat Test</div>
+      <div class="brand-name">Litchat Staging</div>
       <button id="closeSidebarBtn" aria-label="Close menu">✕</button>
     </div>
 
